261fcfa51f4b9c9769fad339c6ce7af6cc541528
ZhiHao-Xiao <120346956+ZhiHao-Xiao@users.noreply.github.com>
2022-12-16 00:09:55 +0800

    Update Curve.cpp

--- ZeroCurve/ZeroCurve/Curve.cpp
+++ ZeroCurve/ZeroCurve/Curve.cpp
@@ -12,6 +12,11 @@ Curve::Curve(long baseDt, int spot, MMCalendar* pMMCal_,
 	processSwaps();
 }
 
+void
+Curve::insertKeyPoint(date dt, DiscountFactorType df) {
+	m_keyPoints[dt] = df;
+}
+
 KeyPoint
 Curve::retrieveKeyPoint(KeyPoints::const_iterator ki) {
 	if (ki == m_keyPoints.end())
@@ -79,6 +84,47 @@ Curve::processFutures() {
 
 void
 Curve::processSwaps() {
+	SwapsCashFlows data;
+	date spotDt = date(m_baseDate);
+	m_pMMCal->addBusDays(spotDt, m_daysToSpot);
+	data.push_back(SwapsCashFlow(spotDt, -100, getDiscountFactor(spotDt)));
+	int multiplier;
+	if (m_pSwapsInput->m_swapsFreq == "Monthly") { multiplier = 12; }
+	else if (m_pSwapsInput->m_swapsFreq == "Quarterly") { multiplier = 4; }
+	else if (m_pSwapsInput->m_swapsFreq == "Semi-Annually") { multiplier = 2; }
+	else if (m_pSwapsInput->m_swapsFreq == "Annually") { multiplier = 1; }
+	for (int i = 0; i < m_pSwapsInput->m_swapsPoints.size(); i++) {
+		int legs = multiplier * stoi(m_pSwapsInput->m_swapsPoints[i].first.substr(0, 2));
+		date preDt = spotDt;
+		for (int j = 0; j < legs; j++) {
+			m_pMMCal->addMonths(spotDt, 12 / multiplier);
+			if (m_pSwapsInput->m_swapsBasis == "ACT/365") {
+				data.push_back(SwapsCashFlow(spotDt, 100 * m_pSwapsInput->m_swapsPoints[i].second * (spotDt - preDt / 365), getDiscountFactor(spotDt)));}
+			else if (m_pSwapsInput->m_swapsBasis == "ACT/360") {
+				data.push_back(SwapsCashFlow(spotDt, 100 * m_pSwapsInput->m_swapsPoints[i].second * (spotDt - preDt / 360), getDiscountFactor(spotDt)));}
+			else if (m_pSwapsInput->m_swapsBasis == "ACT/ACT") {
+				data.push_back(SwapsCashFlow(spotDt, 100 * m_pSwapsInput->m_swapsPoints[i].second * (spotDt - preDt / date::daysInYear(spotDt.year())), getDiscountFactor(spotDt)));
+			}
+			preDt = spotDt;
+		}
+		date lastDt = m_keyPoints.rbegin()->first;
+		double lastdf = m_keyPoints.rbegin()->second;
+		double uBound = 1.0, lBound = 0.0, npv = 0.0, df;
+		int iter = 0;
+		do {
+			npv = 0.0;
+			df = (uBound + lBound) / 2;
+			for (int k = 0; k < data.size(); ++k) {
+				npv += data[k].CF * (data[k].DF == 0.0 ? lastdf * pow(df / lastdf, (data[k].dt - lastDt) / (data.back().dt - lastDt)) : data[k].DF);
+			}
+			(npv > 0.0) ? uBound = df : lBound = df;
+			iter++;
+		} while (abs(npv) > 0.0001 && iter < 1000);
+		for (int k = 0; k < data.size(); ++k) {
+			if (data[k].DF == 0.0) { insertKeyPoint(data[k].dt, lastdf * pow(df / lastdf, (data[k].dt - lastDt) / (data.back().dt - lastDt))); }
+		}
+		data.clear();
+	}
 }
 
 bool
